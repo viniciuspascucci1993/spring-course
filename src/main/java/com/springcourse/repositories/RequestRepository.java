414bafbb7e681ac77ed5add494199123619e4a26
viniciuspascucci1993 <vinicius.pascucci1@gmail.com>
2023-04-13 21:24:15 -0300

    Create RequestRepository

---
+++ src/main/java/com/springcourse/repositories/RequestRepository.java
@@ -0,0 +1,18 @@
+package com.springcourse.repositories;
+
+import com.springcourse.domain.Request;
+import com.springcourse.enums.RequestState;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RequestRepository extends JpaRepository<Request, Long> {
+
+    List<Request> findAllByOwnerId(Long id);
+
+    @Query("UPDATE Request SET requestState = ?2 WHERE id = ?1") 
+    Request updateStatus(Long id, RequestState requestState);
+}
