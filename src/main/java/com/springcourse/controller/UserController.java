1249514fbc2defdc8237d0adf414e1893ea1294f
viniciuspascucci1993 <vinicius.pascucci1@gmail.com>
2023-04-21 11:27:00 -0300

    Create UserController

---
+++ src/main/java/com/springcourse/controller/UserController.java
@@ -0,0 +1,56 @@
+package com.springcourse.controller;
+
+import com.springcourse.domain.User;
+import com.springcourse.dto.UserLoginDTO;
+import com.springcourse.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "users")
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody UserLoginDTO userLoginDTO) {
+        User userLogged = userService.login(userLoginDTO.getEmail(), userLoginDTO.getPassword());
+        return ResponseEntity.ok(userLogged);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable(name = "id") Long id) {
+        User user = userService.getById(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> save(@RequestBody User user) {
+        User createdUser = userService.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getUsersList() {
+        List<User> usersList = userService.getList();
+        return ResponseEntity.ok(usersList);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> update(@PathVariable(name = "id") Long id , @RequestBody User user) {
+        user.setId(id);
+        User updatedUser = userService.updateUser(user);
+        return ResponseEntity.ok(updatedUser);
+
+    }
+
+}
