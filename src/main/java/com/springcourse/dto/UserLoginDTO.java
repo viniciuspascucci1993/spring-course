1f1292d59ceacfb34df69b54d75f85a1d8b49983
viniciuspascucci1993 <vinicius.pascucci1@gmail.com>
2023-04-21 11:26:42 -0300

    Adding dependency web and tomcat

---
+++ src/main/java/com/springcourse/dto/UserLoginDTO.java
@@ -0,0 +1,16 @@
+package com.springcourse.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class UserLoginDTO {
+
+    private String email;
+    private String password;
+}
