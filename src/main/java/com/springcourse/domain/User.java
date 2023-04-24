16f8d20d6ce71068ce88f4446cc4fc26e5c9b6f1
viniciuspascucci1993 <vinicius.pascucci1@gmail.com>
2023-04-24 14:45:41 -0300

    Config role updateble = false

--- src/main/java/com/springcourse/domain/User.java
+++ src/main/java/com/springcourse/domain/User.java
@@ -43,7 +43,7 @@ public class User implements Serializable {
     @Column(length = 100, nullable = false)
     private String password;
 
-    @Column(length = 20, nullable = false)
+    @Column(length = 20, nullable = false, updatable = false)
     @Enumerated(EnumType.STRING)
     private Role role;
 
