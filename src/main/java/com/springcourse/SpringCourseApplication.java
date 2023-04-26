90be272d0af875a56367823aee625b861b5d56d0
viniciuspascucci1993 <vinicius.pascucci1@gmail.com>
2023-04-26 15:01:57 -0300

    Config provides scop for tomcat and Config Deployable for AWS

--- src/main/java/com/springcourse/SpringCourseApplication.java
+++ src/main/java/com/springcourse/SpringCourseApplication.java
@@ -2,9 +2,19 @@ package com.springcourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringCourseApplication {
+public class SpringCourseApplication extends SpringBootServletInitializer {
+
+	/*
+	* Metodo necessário para implantação em servidor externo Ex: AWS
+	* */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringCourseApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCourseApplication.class, args);
